b843ee2573458c471c11f5d4c4a1d93efd0de58a
Candela <candelabaclini@hotmail.com>
2022-11-15 15:00:31 -0300

    Agrego mas cards

--- js/card.js
+++ js/card.js
@@ -3,7 +3,7 @@ function Card(heading, description, image) {
     this.description = description
     this.image = image
 
-    this.appendTo = function () {
+    this.appendTo = function (destinationElement) {
         let card = document.createElement("a")
         card.classList.add("card")
         card.href = "https://sony.com"
@@ -22,6 +22,8 @@ function Card(heading, description, image) {
                 <p>${description}</p>
             </div>
         </article>`
+
+        destinationElement.appendChild(card)
 
     }
 
@@ -44,151 +46,102 @@ const card2 = new Card(
     "img/productos/divisor ambiente 2.jpeg"
 )
 
+const card3 = new Card(
+    "Divisor de ambiente",
+    "Mueble de melamina de alta densidad con estructura metalica negra + perchero para que cuelgues tu ropa!",
+    "img/productos/divisor ambiente 3.jpeg"
+)
+
+const card4 = new Card(
+    "Escritorio",
+    "Mueble de melamina de alta densidad con estructura metalica negra + un cajon y espacio de guardado bajo mesada",
+    "img/productos/escritorio.jpeg"
+)
+
+const card5 = new Card(
+    "Mesita de luz",
+    "Mueble de melamina de alta densidad con estructura metalica negra + cajon de guardado",
+    "img/productos/mesita de luz.jpeg"
+)
+
+const card6 = new Card(
+    "Perchero + mueble de apoyo",
+    "Mueble de melamina de alta densidad con estructura metalica negra + un espacio para que guardes zapatillas",
+    "img/productos/perchero.jpeg"
+)
+
+const card7 = new Card(
+    "Rack de tv chico",
+    "Mueble de melamina de alta densidad con estructura metalica negra + espacios de guardado",
+    "img/productos/Rack chico.jpeg"
+)
+
+const card8 = new Card(
+    "Rack de tv grande",
+    "Mueble de melamina de alta densidad con estructura metalica negra + espacios de guardado",
+    "img/productos/Rack grande.jpeg"
+)
+
+const card9 = new Card(
+    "Recibidor",
+    "Mueble de melamina de alta densidad con estructura metalica negra + espacios de guardo bajo mesada",
+    "img/productos/recibidor.jpeg"
+)
+
+const card10 = new Card(
+    "Recibidor",
+    "Mueble de melamina de alta densidad con estructura metalica negra",
+    "img/productos/guardado de cama.jpeg"
+)
+
+const card11 = new Card(
+    "Biblioteca",
+    "Mueble de melamina de alta densidad con estructura metalica negra, sus estantes son irregulares para aportar un mayor diseño",
+    "img/productos/biblioteca1.jpeg"
+)
+
+const card12 = new Card(
+    "Biblioteca + vinoteca",
+    "Mueble de melamina de alta densidad con estructura metalica negra, tiene la funcion de un divisor de ambientes, cuenta con espacios de guardo y una vinoteca",
+    "/img/productos/"
+)    
+
+
+    
+
+
+
 console.log(card1)
 console.log(card2)
+console.log(card3)
+console.log(card4)
+console.log(card5)
+console.log(card6)
+console.log(card7)
+console.log(card8)
+console.log(card9)
+console.log(card10)
+console.log(card11)
+console.log(card12)
+
+const cards = [
+    card1, 
+    card2,
+    card3,
+    card4,
+    card5,
+    card6,
+    card7,
+    card8,
+    card9,
+    card10,
+    card11,
+    card12
+]
+
+console.log(cards)
 
 
+card1.appendTo(elemCardsContainer)
 
 
-
-/* <a href="#" class="card">
-            <article class="card__article">
-              <div class="card__image-container">
-                <img class="card__image" src="img/productos/divisor ambiente 3.jpeg" alt="Divisor de ambiente"/>
-              </div>
-              <div class="card__content">
-                <h2 class="card__heading">Divisor de ambiente</h2>
-                <div class="card__description">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Odit deleniti consequuntur inventore minus ea consequatur
-                    omnis. Eaque repudiandae amet ad!
-                  </p>
-                </div>
-              </div>
-            </article>
-          </a>
-          <!-- Final class .card -->
-          <a href="#" class="card">
-            <article class="card__article">
-              <div class="card__image-container">
-                <img class="card__image" src="img/productos/escritorio.jpeg" alt="Escritorio"/>
-              </div>
-              <div class="card__content">
-                <h2 class="card__heading">Escritorio</h2>
-                <div class="card__description">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Odit deleniti consequuntur inventore minus ea consequatur
-                    omnis. Eaque repudiandae amet ad!
-                  </p>
-                </div>
-              </div>
-            </article>
-          </a>
-          <!-- Final class .card -->
-          <a href="#" class="card">
-            <article class="card__article">
-              <div class="card__image-container">
-                <img class="card__image" src="img/productos/mesita de luz.jpeg" alt="mesita de luz"/>
-              </div>
-              <div class="card__content">
-                <h2 class="card__heading">Mesita de luz</h2>
-                <div class="card__description">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Odit deleniti consequuntur inventore minus ea consequatur
-                    omnis. Eaque repudiandae amet ad!
-                  </p>
-                </div>
-              </div>
-            </article>
-          </a>
-          <!-- Final class .card -->
-          <a href="#" class="card">
-            <article class="card__article">
-              <div class="card__image-container">
-                <img class="card__image" src="img/productos/perchero.jpeg" alt="Perchero + mueble de apoyo"/>
-              </div>
-              <div class="card__content">
-                <h2 class="card__heading">Perchero + mueble de apoyo</h2>
-                <div class="card__description">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Odit deleniti consequuntur inventore minus ea consequatur
-                    omnis. Eaque repudiandae amet ad!
-                  </p>
-                </div>
-              </div>
-            </article>
-          </a>
-          <!-- Final class .card -->
-          <a href="#" class="card">
-            <article class="card__article">
-              <div class="card__image-container">
-                <img class="card__image" src="img/productos/Rack chico.jpeg" alt="Rack de tv chico"/>
-              </div>
-              <div class="card__content">
-                <h2 class="card__heading">Rack de tv chico</h2>
-                <div class="card__description">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Odit deleniti consequuntur inventore minus ea consequatur
-                    omnis. Eaque repudiandae amet ad!
-                  </p>
-                </div>
-              </div>
-            </article>
-          </a>
-          <!-- Final class .card -->
-          <a href="#" class="card">
-            <article class="card__article">
-              <div class="card__image-container">
-                <img class="card__image" src="img/productos/Rack grande.jpeg" alt="Rack de tv grande"/>
-              </div>
-              <div class="card__content">
-                <h2 class="card__heading">Rack de tv grande</h2>
-                <div class="card__description">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Odit deleniti consequuntur inventore minus ea consequatur
-                    omnis. Eaque repudiandae amet ad!
-                  </p>
-                </div>
-              </div>
-            </article>
-          </a>
-          <a href="#" class="card">
-            <article class="card__article">
-              <div class="card__image-container">
-                <img class="card__image" src="img/productos/recibidor.jpeg" alt="recibidor"/>
-              </div>
-              <div class="card__content">
-                <h2 class="card__heading">Recibidor</h2>
-                <div class="card__description">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Odit deleniti consequuntur inventore minus ea consequatur
-                    omnis. Eaque repudiandae amet ad!
-                  </p>
-                </div>
-              </div>
-            </article>
-          </a>
-          <a href="#" class="card">
-            <article class="card__article">
-              <div class="card__image-container">
-                <img class="card__image" src="img/productos/guardado de cama.jpeg" alt="guardado de cama"/>
-              </div>
-              <div class="card__content">
-                <h2 class="card__heading">Pie de cama</h2>
-                <div class="card__description">
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Odit deleniti consequuntur inventore minus ea consequatur
-                    omnis. Eaque repudiandae amet ad!
-                  </p>
-                </div>
-              </div>
-            </article>
-          </a>  */
